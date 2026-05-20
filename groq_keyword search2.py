ecf29e9bcd507c0293601e5c203912b7b95c5724
Hrithika23 <109800326+Hrithika23@users.noreply.github.com>
2026-05-20 15:28:33 +0530

    Update groq_keyword search2.py

--- groq_keyword search2.py
+++ groq_keyword search2.py
@@ -47,19 +47,7 @@ def create_chunks(text, chunk_size=500, overlap=100):
         start += chunk_size - overlap
 
     return chunks
-# def create_chunks(text, chunk_size=500,overlap=100):
 
-#     words = text.split()
-
-#     chunks = []
-
-#     for i in range(0, len(words), chunk_size):
-
-#         chunk = " ".join(words[i:i + chunk_size])
-
-#         chunks.append(chunk)
-
-#     return chunks
 
 
 # ---------------- KEYWORD SEARCH ----------------
